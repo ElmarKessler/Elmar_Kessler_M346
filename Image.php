5c4d37f8aaf642c69794d6570aa6d111a8664020
Elmar KesslerElmar Kesslergit config --global user.name Elmar <elmar.kessler@noseryoung.com>
2023-11-06 08:28:20 +0100

    update

---
+++ Image.php
@@ -0,0 +1,14 @@
+<?php
+$ihrname = "Elmar Kessler";
+<html>
+<head>
+<title>echo($ihrname)</title>
+</head>
+<body>
+Grossstadt von oben.
+<br />
+<img src="https://images.unsplash.com/photo-1698094276298-92b21f2df7f9?auto=format&fit=crop&q=60&w=1200&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D" />
+Dieses Bild stammt von unsplash.
+</body>
+</html>
+?>
